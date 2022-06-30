32d1aef57ac3107ccf3378aae7f778b5027baf99
SaimorAlam <saimoralam@gmail.com>
2022-06-30 11:05:16 +0600

    Fix API calling issues

--- app/Http/Controllers/NewsApiController.php
+++ app/Http/Controllers/NewsApiController.php
@@ -9,10 +9,22 @@ class NewsApiController extends Controller
 {
     public function news_data()
     {
-        $news_url = file_get_contents('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e67e3f52a34d4b3a890051c783485533');
-        $articles_array = json_decode($news_url, true);
+
+        $headers = [
+            "Content-type: Application/json; charset=UTF-8",
+            "User-Agent: " . $_SERVER['HTTP_USER_AGENT'],
+
+        ];
+        $curl_handle = curl_init();
+        curl_setopt_array($curl_handle, [
+            CURLOPT_URL => "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e67e3f52a34d4b3a890051c783485533",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => $headers,
+        ]);
+        $data = curl_exec($curl_handle);
+        curl_close($curl_handle);
+        $articles_array = json_decode($data, true);
         $articles = $articles_array['articles'];
-        // return view('pages.news',['articles'=> $articles]);
         return view('pages.index',['articles'=> $articles]);
     }
     public function show_news(Request $request)
@@ -25,7 +37,7 @@ class NewsApiController extends Controller
             'content' => $request->input('article-content'),
 
         ];
-        
+
         return view('pages.news', ['articles' => $articles]);
     }
 }
